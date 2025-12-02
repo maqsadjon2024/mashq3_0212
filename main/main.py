6c9fc41c7f8ad99816c887047a93f4ef7a49f507
maqsadjon2024 <maqsadjonqurbondurdiev58@gmail.com>
2025-12-02 17:34:13 +0500

    Create main.py

---
+++ main/main.py
@@ -0,0 +1,24 @@
+# 1-misol
+a_bormi = lambda ism: "a" in ism.lower()
+
+print(a_bormi("MAQSADJON"))
+
+# 2-misol
+bosh_harf = lambda s: s.upper()
+
+print(bosh_harf("maqsadjon"))
+
+# 3-misol
+farq_moduli = lambda x, y: abs(x - y)
+
+print(farq_moduli(17, 12))
+
+# 4-misol
+tengmi = lambda x, y: x == y
+
+print(tengmi(17, 17))
+
+# 5-misol
+ikki_ga_bol = lambda x: x // 2
+
+print(ikki_ga_bol(18))
